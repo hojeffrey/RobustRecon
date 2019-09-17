fd306e5f74caaabb2e23aa1462811bd7c73681e7
hojeffrey <52347174+hojeffrey@users.noreply.github.com>
2019-09-17 10:40:14 -0400

    Update nmap-waf.py

--- nmap-waf.py
+++ nmap-waf.py
@@ -1,29 +1,11 @@
-#!/bin/usr/python3 
+#!/bin/usr/python3
 
 import sys
-#import nmap
-import os 
-#import requests
-#import socket
+import os
 
 print("[+] Checking for WAF...")
 
-'''
-ip = sys.argv[1] 
-#Checks if there is WAF on the webserver
-nmap1= nmap.PortScanner() 
-
-a = nmap1.nmap_version() 
-
-print(a) 
-
-#takes in the scan and argument 
-nm.scan(ip, 
-'''
-
 ip = sys.argv[1]
-#wordlist = sys.argv[2]
-
 
 def start_nmap(options, ip):
         ip = sys.argv[1]
@@ -34,10 +16,3 @@ def start_nmap(options, ip):
 
 print (start_nmap(' -p80 --script http-waf-detect', ip))
 
-'''
-print ("[+] Checking the target ip ..."
-
-try:
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except: print("Error")
-'''
