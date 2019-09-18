346201d60ab7ad535b670286f1f4a1c43174bcad
Jeffrey <jeffreyho320@gmail.com>
2019-09-18 15:33:29 -0400

    directorybruteforce.py updated with function

--- directorybruteforce.py
+++ directorybruteforce.py
@@ -3,35 +3,28 @@
 import sys
 import os 
 import requests
-import socket
 
-print("[+] Checking for WAF...")
+def bruteforce (ip, wordlist):
+	try:
+		f = open(wordlist)
+		wl_check = f.read().strip().split('\n')
+		print (" Finished...")
 
+		print (" Amount of Directory path to check: " + (str(len(wl_check)))) # check this 
+	except IOError: # IOError is an error when input/output is wrong 
+		print ("Something went wrong...") 
+		sys.exit(1) 
 
-ip = sys.argv[1]
-wordlist = sys.argv[2]
- 
-try:
-	f = open(wordlist)
-	wl_check = f.read().strip().split('\n')
-	print (" Finished...")
-
-	print (" Amount of Directory path to check: " + (str(len(wl_check)))) # check this 
-except IOError: # IOError is an error when input/output is wrong 
-	print ("Something went wrong...") 
-	sys.exit(1) 
-
-def pathcheck(path): 
-	try: 
-		response = requests.get("http://" + ip + "/" + path).status_code # makes request to the website using wordlist 
-	except Exception:
-		print (" Error Occured") 
+	def pathcheck(path): 
+		try: 
+			response = requests.get("http://" + ip + "/" + path).status_code # makes request to the website using wordlist 
+		except Exception:
+			print (" Error Occured") 
 		#sys.exit(1)
-	if response == 200: #If server responds 
-		print ("Valid path [200]:" + path ) 
-print ("\n Starting Directory Brute Forcing... \n")
+		if response == 200: #If server responds 
+			print ("Valid path [200]:" + path ) 
 
-for path in (wl_check): 
-	pathcheck(path) #check this
-print ("\n Scan Complete!") 
-
+	for path in (wl_check): 
+		pathcheck(path) #check this
+	print(wl_check)
+	print ("\n Scan Complete!") 
